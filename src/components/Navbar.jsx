3989a96b98b32025fae5a0d6af17dc39529cbe5a
Thato Ramphore <75572984+thatoramphore@users.noreply.github.com>
2024-06-06 21:23:50 +0200

    add Highligts component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
             <nav className="flex w-full screen-max-width">
-                <img src={appleImg} alt="Apple Logo" width={14} height={14}/>
+                <div className="cursor-pointer"><img src={appleImg} alt="Apple Logo" width={12} height={14}/></div>
 
                 <div className="flex flex-1 justify-center max-sm:hidden">
                     {navLists.map((nav,
@@ -18,8 +18,8 @@ const Navbar = () => {
                 </div>
 
                 <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-                    <img src={searchImg} alt="Search Icon" width={18} height={18} />
-                    <img src={bagImg} alt="Bag Icon" width={18} height={18} />
+                    <div className="cursor-pointer"><img src={searchImg} alt="Search Icon" width={18} height={18} /></div>
+                    <div className="cursor-pointer"><img src={bagImg} alt="Bag Icon" width={18} height={18} /></div>
                 </div>
             </nav>
         </header>
